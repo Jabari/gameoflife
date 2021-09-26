8331d5c5812dc73c7383ee4333903f4e543b5e4e
Jabari <jabaricmartin@gmail.com>
2021-09-26 15:34:51 -0700

    fix bug where 2D array in console.log did not match rendered grid

--- index.js
+++ index.js
@@ -14,25 +14,38 @@ var columns = canvas.width / canvas.resolution;
 var rows = canvas.height / canvas.resolution;
 var grid;
 
-(function buildGrid() {
+function buildGrid() {
     grid = new Array(columns).fill(null)
         .map(()=> new Array(rows).fill(null)
             .map(()=> Math.floor(Math.random() * 2)));
-    return grid;
-})();
+    //return grid;
+};
 
-function render(grid) {
+function fillGrid(grid) {
     for (let x = 0; x < columns; x++) {
         for (let y = 0; y < rows; y++) {
-            var cell = grid[x][y];
-            ctx.fillStyle = '#000';
-            if (cell == 1){
-                ctx.fillRect(x*columns, y*rows, columns, rows)
-            } else {
+            var cell = grid[y][x];
+            ctx.rect(x*columns, y*rows, columns, rows);
+            
+            if (cell == 1) {
+                ctx.fillStyle = 'black';
+                ctx.fillRect(x*columns, y*rows, columns, rows);  
                 ctx.strokeRect(x*columns, y*rows, columns, rows);
+            } else {
+                ctx.fillStyle = 'white'
+                ctx.fillRect(x*columns, y*rows, columns, rows);
+                ctx.strokeRect(x*columns, y*rows, columns, rows);
+                
             }
         }
     }
 };
-$('button').on('click',()=> render(grid));
+function showGrid () {
+    buildGrid();
+    fillGrid(grid);
+}
+$('button').on('click',()=> showGrid());
 
+// Any live cell with two or three live neighbours survives.
+// Any dead cell with three live neighbours becomes a live cell.
+// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
