ff0dc83232d5526215d48c49f019a22df2e4a993
Jabari <jabaricmartin@gmail.com>
2021-09-26 18:47:03 -0700

    correct game rules

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var button = document.querySelector('button');
 var canvas = document.querySelector('canvas');
-var ctx;
-var newGeneration;
-var grid;
+var size = $('#universe-size').val();
+var columns, ctx, grid, newGeneration, rows;
+
 if (canvas.getContext) {
     ctx = canvas.getContext('2d');
 } else {
@@ -10,16 +10,16 @@ if (canvas.getContext) {
 }
  
 var resolution = 10;
-canvas.width = 800;
-canvas.height = 800;
-var columns = canvas.width / resolution;
-var rows = canvas.height / resolution;
 
-function buildGrid() {    
+
+function buildGrid() { 
+    canvas.width = $('#universe-size').val();
+    canvas.height = canvas.width;   
+    columns = canvas.width / resolution;
+    rows = canvas.height / resolution;
     grid = new Array(columns).fill(null)
         .map(()=> new Array(rows).fill(null)
             .map(()=> Math.floor(Math.random() * 2)));
-    //return grid;
 };
 function renderGrid(grid) {
     for (let x = 0; x < columns; x++) {
@@ -29,12 +29,12 @@ function renderGrid(grid) {
             
             if (cell == 1) {
                 ctx.fillStyle = 'black';
-                ctx.fillRect(x*columns, y*rows, columns, rows);  
-                ctx.strokeRect(x*columns, y*rows, columns, rows);
+                ctx.fillRect(x*resolution, y*resolution, resolution, resolution);  
+                ctx.strokeRect(x*resolution, y*resolution, resolution, resolution);
             } else {
                 ctx.fillStyle = 'white'
-                ctx.fillRect(x*columns, y*rows, columns, rows);
-                ctx.strokeRect(x*columns, y*rows, columns, rows);
+                ctx.fillRect(x*resolution, y*resolution, resolution, resolution);
+                ctx.strokeRect(x*resolution, y*resolution, resolution, resolution);
                 
             }
         }
@@ -63,12 +63,20 @@ function evolution() {
                     }
                 }
             }
-            if (cell == 1 && numOfNeighbors != 2 ||
-                cell == 1 && numOfNeighbors != 3) {
-                    newGeneration[y][x] = 0;
-            } else if (cell == 0 && numOfNeighbors == 3) {
-                    newGeneration[y][x] = 1;
+
+            if (cell == 1 && numOfNeighbors < 2) {
+                newGeneration[y][x] = 0;
+              } else if (cell === 1 && numOfNeighbors > 3) {
+                newGeneration[y][x] = 0;
+              } else if (cell === 0 && numOfNeighbors === 3) {
+                newGeneration[y][x] = 1;
             }
+            // if (cell == 1 && numOfNeighbors != 2 ||
+            //     cell == 1 && numOfNeighbors != 3) {
+            //         newGeneration[y][x] = 0;
+            // } else if (cell == 0 && numOfNeighbors == 3) {
+            //         newGeneration[y][x] = 1;
+            // }
                     
         }
     }
@@ -89,6 +97,7 @@ function showGrid () {
 $('#build').on('click',()=> showGrid());
 $('#evolve').on('click',()=> updateGrid());
 
-// Any live cell with two or three live neighbours survives.
-// Any dead cell with three live neighbours becomes a live cell.
-// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any live cell with more than three live neighbours dies, as if by overpopulation.
+// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
